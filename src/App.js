107878b9220d3c7cd335e868cd428024e9f3943f
MartaKnitter <55092067+MartaKnitter@users.noreply.github.com>
2023-01-10 12:44:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Box header="asd">dupa2</Box>
+    </div>
+  );
+}
+
+const Box = ({ header, children }) => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <>
+      <h2>{header}</h2>
+      <h3>Counter: {counter}</h3>
+      <button onClick={() => setCounter(counter + 1)}>Set counter</button>
+      <div>{children}</div>
+    </>
+  );
+};
